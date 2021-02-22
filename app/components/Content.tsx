79ab9c8fd824b2a8d5c5297c981f9168eb28338d
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-02-22 15:39:30 +0100

    added style

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import SectionItem from './SectionItem';
 
 export default function Content() {
     return(
-        <>
+        <div style={contentBox}>
             <SectionItem />
             <SectionItem />
             <SectionItem />
-        </>
+        </div>
     )
+};
 
+const contentBox: CSSProperties = {
+    background: 'purple',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '80%'
 }
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 
 export default function Navbar() {
     return(
-        <h1>Navbar</h1>
-    )
+        <div style={navbar}>
+            <h1 style={title}>Navbar</h1>
+        </div>
+    );
 }
+
+const navbar: CSSProperties = {
+    height: '20%',
+    background: 'purple',
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: '1rem'
+};
+
+const title: CSSProperties = {
+    fontSize: '2rem',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    cursor: 'pointer',
+    margin: 0,
+    padding: 0
+};
