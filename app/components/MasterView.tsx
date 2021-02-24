23e8da6b3043f028a85bc0aea7e8620b2e5e0799
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-02-24 11:03:11 +0100

    added master view functions

--- app/components/MasterView.tsx
+++ app/components/MasterView.tsx
@@ -2,11 +2,12 @@ import React, { CSSProperties } from 'react';
 import SectionItem from './SectionItem';
 
 export default function MasterView() {
+
+    const sectionIds = ['forest', 'sky', 'desert'];
+
     return(
         <div style={contentBox}>
-            <SectionItem />
-            <SectionItem />
-            <SectionItem />
+            {sectionIds.map((value) => <SectionItem id={value} />)}
         </div>
     )
 };
@@ -16,6 +17,8 @@ const contentBox: CSSProperties = {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-between',
-    height: '80%',
-    alignItems: 'center'
+    height: '100%',
+    margin: '0.5rem',
+    justifyItems: 'stretch',
+    alignItems: 'stretch'
 }
--- app/components/SectionItem.tsx
+++ app/components/SectionItem.tsx
@@ -1,20 +1,52 @@
 import React, {CSSProperties} from 'react';
 
-export default function SectionItem() {
+interface Props {
+    id: string
+}
+
+export default function SectionItem(props: Props) {
+    const imageSrc = `../assets/${props.id}.jpg`;
+
     return(
-        <div>
-            <div style={title}>item title</div>
-            <div style={imageDiv}></div>
+        <div style={{ ...gridItem, ...centeredContent }}>
+            <img src={imageSrc} style={fullscreen} />
+            <h2 style={{ ...centeredAbsolute, ...title}}>{props.id}</h2>
         </div>
     )
 }
 
 const title: CSSProperties = {
-    fontSize: '2rem'
+    fontSize: '2rem',
+    color: '#1E1E1E',
+    textShadow: '0 0 3px white'
 }
 
-const imageDiv: CSSProperties = {
-    height: '3rem',
-    width: '3rem',
-    background: 'red'
+const gridItem: CSSProperties = {
+    position: 'relative',
+    margin: '0.5em',
+    background: '#808080',
+    height: '100%'
 }
+
+const fullscreen: CSSProperties = {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+    position: 'absolute'
+}
+
+const centeredAbsolute: CSSProperties = {
+    position: 'absolute',
+    margin: 0,
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)'
+}
+
+const centeredContent: CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center'
+};
